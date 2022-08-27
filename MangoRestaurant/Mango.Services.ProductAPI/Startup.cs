e37e972ff13a3bcfff533f327bd5cf1dd824e69e
Summy Singh <summysingh86@gmail.com>
2022-08-27 14:35:43 +0530

    working with DTOs and mapping

--- MangoRestaurant/Mango.Services.ProductAPI/Startup.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Startup.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -31,6 +33,10 @@ namespace Mango.Services.ProductAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
